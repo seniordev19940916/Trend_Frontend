aeb900563f49b2ef4f99a00429f0a582e0489fea
axel <axel.svanfeldt@gmail.com>
2019-08-16 18:07:09 +0200

    Added locations

--- server/config/locations.js
+++ server/config/locations.js
@@ -2,13 +2,11 @@
 
 const locations = [
     {location: "Australia", woeid: 23424748, iso: "AU"},
-    //{location: "Argentine", woeid: 23424747, iso: "AR"},
     {location: "Brazil", woeid: 23424768, iso: "BR"},
     {location: "Canada", woeid: 23424775, iso: "CA"},
-    //{location: "France", woeid: 23424819, iso: "FR"},
+    {location: "France", woeid: 23424819, iso: "FR"},
     {location: "Germany", woeid: 23424829, iso: "DE"},
-    /*{location: "India", woeid: 23424848, iso: "IN"},
-    //{location: "Italy", woeid: 23424853, iso: "IT"},
+    {location: "India", woeid: 23424848, iso: "IN"},
     {location: "Japan", woeid: 23424856, iso: "JP"},
     {location: "Mexico", woeid: 23424900, iso: "MX"},
     {location: "Russia", woeid: 23424936, iso: "RU"},
@@ -16,7 +14,7 @@ const locations = [
     {location: "Sweden", woeid: 23424954, iso: "SE"},
     {location: "Turkey", woeid: 23424969, iso: "TR"},
     {location: "United Kingdom", woeid: 23424975, iso: "GB"},
-    {location: "United States", woeid: 23424977, iso: "US"},*/
+    {location: "United States", woeid: 23424977, iso: "US"},
     {location: "Worldwide", woeid: 1, iso: ""}
 ];
 
