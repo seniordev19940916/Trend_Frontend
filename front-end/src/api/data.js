400511699f727dc3f2c898d9bff7603b6e7c7bb1
seniordev19940916 <mainwork19940916@outlook.com>
2023-11-16 15:23:00 -0500

    upgrade front-end

--- front-end/src/api/data.js
+++ front-end/src/api/data.js
@@ -1,16 +1,19 @@
 // API requests to get data from the server
 
-const axios = require('axios');
+const axios = require("axios");
 
 const api = {
-    getData: (endpoint) => {
-        const url = `http://localhost:3001/api/${endpoint}`; // Get port from environment variables
-        return axios.get(url).then(response => {
-            return response.data
-        }).catch(error => {
-            return {success: false, error: error};
-        });
-    }
+  getData: (endpoint) => {
+    const url = `http://localhost:3001/api/${endpoint}`; // Get port from environment variables
+    return axios
+      .get(url)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        return { success: false, error: error };
+      });
+  },
 };
 
 export default api;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "../src/redux/store";
+import { Auth0Provider } from "@auth0/auth0-react";
+// import Auth from "./routes/Auth";
 //import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Auth0Provider
+        domain="dev-2qz2wjgdc2owecxm.us.auth0.com"
+        clientId="Cx0rKVgdTfUWmS1h3pciRWpoY5jvOWi5"
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
