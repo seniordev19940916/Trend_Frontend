cc8330769a67d74455a909cfbad9aafbe75b4e9d
seniordev19940916 <mainwork19940916@outlook.com>
2023-11-16 20:27:46 -0500

    redirect_url to window.location.origin

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
       <Auth0Provider
         domain="dev-2qz2wjgdc2owecxm.us.auth0.com"
         clientId="Cx0rKVgdTfUWmS1h3pciRWpoY5jvOWi5"
-        redirectUri="https://trend-frontend.vercel.app/app"
+        redirectUri={window.location.origin}
       >
         {/* <App /> */}
         <LandingPage />
