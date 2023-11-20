3a9d9183746acec22c933bafe7963b129ba50b66
seniordev19940916 <mainwork19940916@outlook.com>
2023-11-20 02:21:05 -0500

    update Auth0 config add login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import SmoothScroll from "smooth-scroll";
-import JsonData from "../data/data.json";
-import { Navigation } from "../landingComponent/navigation";
-import { Header } from "../landingComponent/header";
-import { Features } from "../landingComponent/features";
-import { About } from "../landingComponent/about";
-import { Services } from "../landingComponent/services";
-import { Gallery } from "../landingComponent/gallery";
-import { Testimonials } from "../landingComponent/testimonials";
-import { Team } from "../landingComponent/Team";
-import { Contact } from "../landingComponent/contact";
 import "./Login.css";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
@@ -20,30 +10,18 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
 
 const Login = () => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
+    <div>
       {isAuthenticated ? (
-        <button style={{ color: "#c28585" }} onClick={() => logout()}>
-          Log out
+        <button className="login-button" onClick={() => logout()}>
+          <text className="btn">Log out</text>
         </button>
       ) : (
         <button onClick={() => loginWithRedirect()} className="login-button">
           <text className="btn">Log In</text>
         </button>
       )}
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
     </div>
   );
 };
